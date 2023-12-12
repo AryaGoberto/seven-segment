4c6d040f24144ff9b87f20b45c0ed3c2715f0cd9
Mutualism <arguna2019@gmail.com>
2023-12-12 11:38:18 +0800

    merapikan css dan merubah tampilan menjadi lebih fresh

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-    function displayDigit(digit) {
-        // Define the segments for each digit
-        const segments =    [
-        [1, 1, 1, 0, 1, 1, 1], // 0
-        [0, 1, 1, 0, 0, 0, 0], // 1
-        [1, 1, 0, 1, 1, 0, 1], // 2
-        [1, 1, 1, 1, 0, 0, 1], // 3
-        [0, 1, 1, 1, 0, 1, 0], // 4
-        [1, 0, 1, 1, 0, 1, 1], // 5
-        [1, 0, 1, 1, 1, 1, 1], // 6
-        [1, 1, 1, 0, 0, 0, 0], // 7
-        [1, 1, 1, 1, 1, 1, 1], // 8
-        [1, 1, 1, 1, 0, 1, 1]  // 9
-        ];
+// Define the segments for each digit
+const segments = [
+    [1, 1, 1, 0, 1, 1, 1], // 0
+    [0, 1, 1, 0, 0, 0, 0], // 1
+    [1, 1, 0, 1, 1, 0, 1], // 2
+    [1, 1, 1, 1, 0, 0, 1], // 3
+    [0, 1, 1, 1, 0, 1, 0], // 4
+    [1, 0, 1, 1, 0, 1, 1], // 5
+    [1, 0, 1, 1, 1, 1, 1], // 6
+    [1, 1, 1, 0, 0, 0, 0], // 7
+    [1, 1, 1, 1, 1, 1, 1], // 8
+    [1, 1, 1, 1, 0, 1, 1]  // 9
+];
 
-        for (let i = 0; i < 9; i++) {
-            const segmentID = "segment" + String.fromCharCode(65 + i);
-            const segment = document.getElementById(segmentID);
-            segment.style.backgroundColor = segments[digit][i] ? "red" : "gray";
-            const displayArea = document.getElementById("tampilanBiner");
-            displayArea.innerHTML = "Biner ke- " + digit + " : " + segments[digit];
-        };
-
-
+// Function to display a digit on segments
+function displayDigit(digit) {
+    for (let i = 0; i < 7; i++) {
+        const segmentID = "segment" + String.fromCharCode(65 + i);
+        const segment = document.getElementById(segmentID);
+        // Set background color based on segment status
+        segment.style.backgroundColor = segments[digit][i] ? "red" : "black";
     }
-    function clearDisplay() {
-        for (let i = 0; i < 9; i++) {
-            const segmentID = "segment" + String.fromCharCode(65 + i);
-            const segment = document.getElementById(segmentID);
-            segment.style.backgroundColor = "gray";
-        }
+    const displayArea = document.getElementById("tampilanBiner");
+    // Update display area with binary information
+    displayArea.innerHTML = "Biner ke-  " + digit + " : " + segments[digit].join(", ");
+}
+
+// Function to clear the display area
+function clearDisplay() {
+    for (let i = 0; i < 7; i++) {
+        const segmentID = "segment" + String.fromCharCode(65 + i);
+        const segment = document.getElementById(segmentID);
+        // Reset background color to black
+        segment.style.backgroundColor = "black";
     }
+
+    const displayArea = document.getElementById("tampilanBiner");
+    // Clear the content of the display area
+    displayArea.innerHTML = "";
+}
