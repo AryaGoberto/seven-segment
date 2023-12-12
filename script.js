75817708911851fa9b8788354045616c7b75e48b
Mutualism <arguna2019@gmail.com>
2023-12-12 22:19:34 +0800

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Define the segments for each digit
 const segments = [
     [1, 1, 1, 0, 1, 1, 1], // 0
     [0, 1, 1, 0, 0, 0, 0], // 1
@@ -12,29 +11,23 @@ const segments = [
     [1, 1, 1, 1, 0, 1, 1]  // 9
 ];
 
-// Function to display a digit on segments
 function displayDigit(digit) {
     for (let i = 0; i < 7; i++) {
         const segmentID = "segment" + String.fromCharCode(65 + i);
         const segment = document.getElementById(segmentID);
-        // Set background color based on segment status
         segment.style.backgroundColor = segments[digit][i] ? "red" : "black";
     }
     const displayArea = document.getElementById("tampilanBiner");
-    // Update display area with binary information
     displayArea.innerHTML = "Biner ke-  " + digit + " : " + segments[digit].join(", ");
 }
 
-// Function to clear the display area
 function clearDisplay() {
     for (let i = 0; i < 7; i++) {
         const segmentID = "segment" + String.fromCharCode(65 + i);
         const segment = document.getElementById(segmentID);
-        // Reset background color to black
         segment.style.backgroundColor = "black";
     }
 
     const displayArea = document.getElementById("tampilanBiner");
-    // Clear the content of the display area
     displayArea.innerHTML = "";
 }
